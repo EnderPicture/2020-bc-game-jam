d97b7184b1e8471491da69559797dc015577e03c
Donny Wu <enderpicture@gmail.com>
2020-02-15 01:52:53 -0800

    bad player movement

---
+++ Assets/Helper.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Helper
+{
+    public static float Map(this float value, float from1, float from2, float to1, float to2)
+    {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+    }
+}
