88ee5be2bd263b0a928930358a16238c4b0e8a56
Donny Wu <enderpicture@gmail.com>
2020-02-15 12:18:42 -0800

    recoil added

--- Assets/ArmController.cs
+++ Assets/ArmController.cs
@@ -8,9 +8,10 @@ public class ArmController : MonoBehaviour
     public float coolDown;
     public Transform bulletsContainer;
     public Transform spawnPoint;
+    public Rigidbody rb;
     float lastShot;
 
-    
+
 
     void Start()
     {
@@ -19,13 +20,17 @@ public class ArmController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
         float time = Time.realtimeSinceStartup;
-        if(Input.GetMouseButton(0) && time > lastShot + coolDown){
+        if (Input.GetMouseButton(0) && time > lastShot + coolDown)
+        {
             lastShot = time;
 
             GameObject newBullet = GameObject.Instantiate(bullet, spawnPoint.position, transform.rotation, bulletsContainer);
             BulletController bulletController = newBullet.GetComponent<BulletController>();
+            Vector2 force = Helper.AngleVector((transform.eulerAngles.z - 90) * Mathf.Deg2Rad);
+
+            rb.AddForce(force, ForceMode.Impulse);
         }
     }
 }
