8397d0ceac190788c96c9d343e54ff40ac0129d2
Donny Wu <enderpicture@gmail.com>
2020-02-15 10:29:49 -0800

    movement added + arm to pointer

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,81 +5,50 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody rb;
-    Vector2 lastDirection;
+    Vector2 lastInput;
 
-    public float boostX;
-    public float accX;
-    public float maxSpeedX;
-    public float dampX;
+    public float maxMove;
+    public float accMove;
+    public float dampMove;
 
-    public float boostJump;
-    public float accJump;
-    public float accJumpDuration;
-
-    float attackTimer;
-
-    float repairTimer;
-
-    float lastAttacked;
-
-    float lastJump;
+    public Transform arm;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        lastJump = Time.realtimeSinceStartup;
-        lastAttacked = Time.realtimeSinceStartup;
+
+        maxMove = maxMove > 0 ? maxMove : 1;
+        accMove = accMove > 0 ? accMove : 1;
     }
     void Update()
     {
-        // Attack Timing
-        if (attackTimer > 0)
-            attackTimer -= Time.deltaTime;
-        if (repairTimer > 0)
-            repairTimer -= Time.deltaTime;
-    } // Update 
+        Vector2 mousePosition = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        float angle = Vector3.SignedAngle(mousePosition, Vector3.up, Vector3.back);
+        arm.eulerAngles = new Vector3(0,0,angle);
+    }
     void FixedUpdate()
     {
-    // Movement 
         float vertical = Input.GetAxisRaw("Vertical");
         float horizontal = Input.GetAxisRaw("Horizontal");
-    
-        Vector2 direction = new Vector2(0, 0);
+
+        Vector2 input = new Vector2(horizontal, vertical);
         Vector3 velocity = rb.velocity;
 
-        if (horizontal > 0) {
-            direction.x = 1;
-        } else if (horizontal < 0) {
-            direction.x = -1;
-        }
+        input = input.normalized;
 
-        if (vertical > 0)
-            direction.y = 1;
-        else if (vertical < 0)
-            direction.y = -1;
+        Vector2 targetVelocity = input * maxMove;
+        Vector2 deltaVelocity = targetVelocity - new Vector2(velocity.x, velocity.y);
 
-        direction = direction.normalized;
-
-    // Movement Feeling
-        if (direction.x != lastDirection.x && direction.x != 0) {
-            velocity.x = boostX * direction.x;
-        } else if (direction.x != 0) {
-            rb.AddForce(accX * direction.x * Mathf.Clamp(maxSpeedX - (velocity.x * direction.x), 0, maxSpeedX), 0, 0);
+        if (input.magnitude > 0) {
+            deltaVelocity *= accMove;
+            rb.AddForce(deltaVelocity.x, deltaVelocity.y, 0);
         } else {
-            rb.AddForce(-velocity.x * dampX, 0, 0);
+            deltaVelocity *= dampMove;
+            rb.AddForce(deltaVelocity.x, deltaVelocity.y, 0);
         }
-
-        if (direction.y != lastDirection.y && direction.y != 0) {
-            velocity.y = boostX * direction.y;
-        } else if (direction.y != 0) {
-            rb.AddForce(0, accX * direction.y * Mathf.Clamp(maxSpeedX - (velocity.y * direction.y), 0, maxSpeedX), 0);
-        } else {
-            rb.AddForce(0, -velocity.y * dampX, 0);
-        }
-
 
         rb.velocity = velocity;
-        lastDirection = direction;
-    } // FixedUpdate
+        lastInput = input;
+    }
 
 }
