e8905f0bf309fe43141165e9267d1f97696b096a
Astellarius <zak.lake0@gmail.com>
2020-02-16 15:06:03 -0800

    healthbar

---
+++ Assets/heathBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heathBar : MonoBehaviour
+{
+
+    public heathBar[] healths;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerController controller = GetComponent<PlayerController>();
+
+        if (controller.health < 20f)
+        {
+            Debug.Log("health bar should die");
+            gameObject.SetActive(false);
+        }
+    }
+}
