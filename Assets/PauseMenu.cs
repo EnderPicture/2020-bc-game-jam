7008152538e805a180f6e22135c93805f970a25d
Andy W <wuandyw@sfu.ca>
2020-02-16 00:53:19 -0800

    Added more pausing.

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -5,11 +5,13 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
+    GameObject[] pauseObjects;
 
     public GameObject pauseMenuUi;
     // Update is called once per frame
 
     void Start () {
+        pauseObjects = GameObject.FindGameObjectsWithTag("Pause");
         pauseMenuUi.SetActive(false);
     }
     void Update()
@@ -29,12 +31,18 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUi.SetActive(false);
         Time.timeScale = 1;
         GameIsPaused = false;
+        foreach(GameObject p in pauseObjects) {
+            p.SetActive(true);
+        }
     }
 
     void Pause() {
         pauseMenuUi.SetActive(true);
         Time.timeScale = 0;
         GameIsPaused = true;
+        foreach(GameObject p in pauseObjects) {
+            p.SetActive(false);
+        }
     }
 
     public void LoadScene(string sceneName)
