b71e1742724e2e51035d9274fa37f27119a492b7
Donny Wu <enderpicture@gmail.com>
2020-02-15 18:22:36 -0800

    images added

---
+++ Assets/InfluencerBubbleController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfluencerBubbleController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    List<GameObject> stuffInBubble = new List<GameObject>();
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(stuffInBubble.Count);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            stuffInBubble.Add(other.gameObject);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        stuffInBubble.Clear();
+    }
+
+}
