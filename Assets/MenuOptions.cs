b5f61da700d6d79591c489b9ea2a03f2254b84cf
Andy W <wuandyw@sfu.ca>
2020-02-15 23:29:20 -0800

    Added Main Menu

---
+++ Assets/MenuOptions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuOptions : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        Debug.Log("Yikes");
+        Application.Quit();
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
